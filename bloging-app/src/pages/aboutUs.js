718db02c087b960ef09ca16987c37690f1579cae
Aditya Wadekar <99280159+xadityaxyz@users.noreply.github.com>
2024-11-10 14:54:25 +0530

    Update aboutUs.js

--- bloging-app/src/pages/aboutUs.js
+++ bloging-app/src/pages/aboutUs.js
@@ -4,7 +4,7 @@ const AboutUs=()=>{
     return (
 
         <div>
-            <h1>This is ABOUTS pages</h1>
+            <h1>Aditya Wadekar</h1>
             <h1>This is about us pages</h1>
         </div>
 
